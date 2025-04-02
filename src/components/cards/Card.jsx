a18ab2e5ec217703ff599c688401b8d5db4f9213
Daniel <danie.p.usb@gmail.com>
2025-04-02 02:29:06 +0200

    feat: add style remember to check package.json and run npm install

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+//import styles from "./Card.modules.sass"
+
+const Card = () => {
+    return (
+        <div className="card">I'm a card</div>
+        //<div className={styles.Card}>I'm a card</div>
+    )
+}
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import Counter from "./components/Counter_clase3.jsx";
 import ToggleButton from "./components/ToggleButton_clase_5.jsx";
 import NameForm from "./components/NameForm_class6.jsx";
 import CounterWithEffect from "./components/Counter_clase7.jsx";
+import Card from "./components/cards/Card.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Card />
     <CounterWithEffect  />
     <NameForm  />
     <ToggleButton  />
